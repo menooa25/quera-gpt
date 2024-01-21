9d333286b2c5ae62a79ba7307f8d690c3b3d0717
menooa25 <menooa2013@gmail.com>
2024-01-21 23:06:22 +0330

    cleaning title after changing tab

--- app/(overview)/Ticket/TabBtn.tsx
+++ app/(overview)/Ticket/TabBtn.tsx
@@ -2,16 +2,21 @@
 
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
+import { useContext } from "react";
+import { TitleContext } from "./TitleContextProvider";
 
 interface Props {
   title: string;
 }
 const TabBtn = ({ title }: Props) => {
   const tabBane = useSearchParams().get("tab");
+  const { setTitle } = useContext(TitleContext);
+
   const active = tabBane === title;
   return (
     <Link
       href={`/?tab=${title}`}
+      onClick={() => !active && setTitle("")}
       className={`flex-1 text-center cursor-pointer py-2 px-4 ${
         active && "text-primary-600  border-b-2 border-primary-500"
       }`}
--- app/(overview)/Ticket/Ticket.tsx
+++ app/(overview)/Ticket/Ticket.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 import { useRouter, useSearchParams } from "next/navigation";
+import { Suspense, useEffect } from "react";
 import Cart from "../../components/Cart";
+import QAGPT from "./QAGPT/QAGPT";
 import QATeacherForm from "./QATeacher/QATeacherForm";
 import TabBtn from "./TabBtn";
-import { Suspense, useEffect } from "react";
 import TitleContextProvider from "./TitleContextProvider";
-import QAGPT from "./QAGPT/QAGPT";
 
 const Ticket = () => {
   const tabName = useSearchParams().get("tab");
@@ -18,26 +18,26 @@ const Ticket = () => {
   }, []);
   return (
     <div className="p-6">
-      <Cart title="درخواست پشتیبانی">
-        <p className="my-3 p-3 text-sm leading-[23.8px] font-normal">
-          بررسی کدی که نوشتید و رفع خطا توسط خودتون یکی از مفیدترین کارها برای
-          پیشرفت مهارت برنامه‌نویسیه! اگر به اندازه کافی تلاش کردید و وقت خوبی
-          برای رفع خطا گذاشتید و به نتیجه نرسیدید، ما اینجاییم که در فرایند
-          دیباگ کردن بهتون کمک کنیم.
-        </p>
-        <div className="rounded-lg overflow-hidden text-lg font-bold text-qu-gray-600 flex items-center shadow-md">
-          <Suspense>
-            <TabBtn title={QAGptLable} />
-            <TabBtn title={QATeacherLable} />
-          </Suspense>
-        </div>
-        <div className="my-5 mx-3">
-          <TitleContextProvider>
+      <TitleContextProvider>
+        <Cart title="درخواست پشتیبانی">
+          <p className="my-3 p-3 text-sm leading-[23.8px] font-normal">
+            بررسی کدی که نوشتید و رفع خطا توسط خودتون یکی از مفیدترین کارها برای
+            پیشرفت مهارت برنامه‌نویسیه! اگر به اندازه کافی تلاش کردید و وقت خوبی
+            برای رفع خطا گذاشتید و به نتیجه نرسیدید، ما اینجاییم که در فرایند
+            دیباگ کردن بهتون کمک کنیم.
+          </p>
+          <div className="rounded-lg overflow-hidden text-lg font-bold text-qu-gray-600 flex items-center shadow-md">
+            <Suspense>
+              <TabBtn title={QAGptLable} />
+              <TabBtn title={QATeacherLable} />
+            </Suspense>
+          </div>
+          <div className="my-5 mx-3">
             {tabName === QATeacherLable && <QATeacherForm />}
             {tabName === QAGptLable && <QAGPT />}
-          </TitleContextProvider>
-        </div>
-      </Cart>
+          </div>
+        </Cart>
+      </TitleContextProvider>
     </div>
   );
 };
